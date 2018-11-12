9281e8740b65536bfcfe205c6b3f888a1e38c93a
Melanie Robinson <melrobinson1992@yahoo.com>
2018-11-12 16:25:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,5 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return "I can\'t hear you!"
-}
-function sayHiToGrandma(string) {
-  return "I love you, too."
+  return "YES INDEED!"
 }
