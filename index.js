773aa812baeb8660d4ee64bd12c8d9ccb206de35
Melanie Robinson <melrobinson1992@yahoo.com>
2018-11-12 16:33:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function whisper(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+ sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
